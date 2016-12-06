44a7e77b4cf6befeb43cbe19b63c369009c8dc56
lilaizhen <li_laizhen@wentianxia.net>
2016-12-06 15:44:35 +0800

    测试了一个东东

--- src/main/java/com/liaiai/k10/KafkaProNewApi.java
+++ src/main/java/com/liaiai/k10/KafkaProNewApi.java
@@ -1,18 +1,10 @@
 package com.liaiai.k10;
 
-import com.alibaba.fastjson.JSON;
-import com.liaiai.ChargeResponseVo;
 import org.apache.kafka.clients.producer.*;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by lilaizhen on 16/9/6.
- */
+
 public class KafkaProNewApi {
     public static void send() throws InterruptedException {
         Properties props = new Properties();
@@ -26,43 +18,19 @@ public class KafkaProNewApi {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for(int i = 0; i < 100; i++){
-            ChargeResponseVo chargeResponseVo=new ChargeResponseVo();
-            chargeResponseVo.setPayerId("10000000af797eb4d6a84e7ab8029d6883739c3410000212");
-            chargeResponseVo.setAmount(new BigDecimal("12.1"));
-            chargeResponseVo.setOrderId(100000462l);
-            chargeResponseVo.setLiveUserId("10000000af797eb4d6a84e7ab8029d6883739c3410000212");
-            chargeResponseVo.setPayHeadImg("http://test.fada.cim/qdja/skaldksaldk.jpg");
-            chargeResponseVo.setSettingId("dda");
-            chargeResponseVo.setDistribution("asda");
-            chargeResponseVo.setAppId("wtx");
-
-            ChargeResponseVo chargeResponseVo2=new ChargeResponseVo();
-            chargeResponseVo2.setPayerId("10000000af797eb4d6a84e7ab8029d6883739c3410000212");
-            chargeResponseVo2.setAmount(new BigDecimal("12.1"));
-            chargeResponseVo2.setOrderId(100000462l);
-            chargeResponseVo2.setLiveUserId("10000000af797eb4d6a84e7ab8029d6883739c3410000212");
-            chargeResponseVo2.setPayHeadImg("http://test.fada.cim/qdja/skaldksaldk.jpg");
-            chargeResponseVo2.setSettingId("dda");
-            chargeResponseVo2.setDistribution("asda");
-            chargeResponseVo2.setAppId("wtx");
-
-            List<ChargeResponseVo> chargeResponseVos=new ArrayList<>();
-            chargeResponseVos.add(chargeResponseVo);
-            chargeResponseVos.add(chargeResponseVo2);
-            ProducerRecord<String,String> record = new ProducerRecord<>("test-01","1", JSON.toJSONString(chargeResponseVos));
+        String data="{taskId: 164,tokenArray: [" +
+                "\"308187f0904411e6830419bbff96e759\",\"dsarewfafas\",\"3erdasd\"]" +
+                "}";
+        ProducerRecord<String,String> record = new ProducerRecord<>("#","1", data);
             producer.send(record,
-                    new Callback() {
-                        public void onCompletion(RecordMetadata metadata, Exception e) {
-                            if(e != null){
-                                e.printStackTrace();
-                            }
-                            System.out.println("The offset of the record we just sent is: " + metadata.offset());
+                    (metadata, e) -> {
+                        if(e != null){
+                            e.printStackTrace();
                         }
+                        System.out.println("The offset of the record we just sent is: " + metadata.offset());
                     });
             TimeUnit.SECONDS.sleep(1);
 //            producer.send(new ProducerRecord<>("bar", Integer.toString(i), Integer.toString(i)));
-        }
         producer.close();
 
     }
